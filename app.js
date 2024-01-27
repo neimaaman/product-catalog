02dd1fcac2600ac6158eea69e89cf4078f1ea2ac
neimaaman <neimaaman1234@gmail.com>
2024-01-27 00:04:51 -0500

    created variables and declared it and created multidimentional arrays

---
+++ app.js
@@ -0,0 +1,99 @@
+// i will declare variables
+// const productName = prompt("enter productName");
+// const model = prompt("enter model");
+// const cost = prompt("enter cost");
+// const qty = prompt("enter qty");
+
+// const productName2 = prompt("enter productName");
+// const model2 = prompt("enter model");
+// const cost12 = prompt("enter cost");
+// const qty2 = prompt("enter qty2");
+
+// const productName3 = prompt("enter productName");
+// const model3 = prompt("enter model");
+// const cost3 = prompt("enter cost");
+// const qty23= prompt("enter qty3");
+
+// const productName4 = prompt("enter productName");
+// const model4 = prompt("enter model");
+// const cost4 = prompt("enter cost");
+// const qty4 = prompt("enter qty");
+
+
+// const productName5 = prompt("enter productName");
+// const model5 = prompt("enter model");
+// const cost5 = prompt("enter cost");
+// const qty5 = prompt("enter qty");
+
+
+
+productCatalog = [];
+console.log(productCatalog);
+
+productCatalog.push(productName);
+console.log(productCatalog);
+productCatalog.push(model);
+console.log(productCatalog);
+productCatalog.push(cost);
+console.log(productCatalog);
+productCatalog.push(qty)
+console.log(productCatalog);
+
+
+productCatalog.push(productName2);
+console.log(productCatalog);
+productCatalog.push(model2);
+console.log(productCatalog);
+productCatalog.push(cost2);
+console.log(productCatalog);
+productCatalog.push(qty2)
+console.log(productCatalog);
+
+
+productCatalog.push(productName3);
+console.log(productCatalog);
+productCatalog.push(model3);
+console.log(productCatalog);
+productCatalog.push(cost3);
+console.log(productCatalog);
+productCatalog.push(qty3)
+console.log(productCatalog);
+
+
+productCatalog.push(productName4);
+console.log(productCatalog);
+productCatalog.push(model4);
+console.log(productCatalog);
+productCatalog.push(cost4);
+console.log(productCatalog);
+productCatalog.push(qty4)
+console.log(productCatalog);
+
+
+productCatalog.push(productName5);
+console.log(productCatalog);
+productCatalog.push(model5);
+console.log(productCatalog);
+productCatalog.push(cost5);
+console.log(productCatalog);
+productCatalog.push(qty5)
+console.log(productCatalog);
+
+
+productCatalog2[
+    [productName, model, cost, qty];
+    [productName2, model2, cost2, qty2];
+    [productName3, model3, cost3, qty3];
+    [productName4, model4, cost4, qty4];
+    [productName5, model5, cost4, qty5];
+
+]
+console.log(productCatalog2);
+
+for 
+
+
+
+
+
+
